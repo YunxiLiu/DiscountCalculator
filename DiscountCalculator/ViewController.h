ddce06e6b81e2d1432344c33289c4fb578f93e5e
YunxiLiu <yunxiliu@usc.edu>
2017-10-05 17:59:19 -0700

    First commit.

---
+++ DiscountCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DiscountCalculator
+//
+//  Created by Yunxi Liu on 6/24/17.
+//  Copyright © 2017 Yunxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DiscountCalculator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DiscountCalculator
+//
+//  Created by Yunxi Liu on 6/24/17.
+//  Copyright © 2017 Yunxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
